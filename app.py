454a706eab29ed0111d349731247d4deca1eb8dd
Marlena <marlena_duda@onet.eu>
2026-04-09 12:09:56 +0200

    Added more text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.Div("Hello from Dash on Azure")
+app.layout = html.Div("Hello from Dash on Azure. Adding more text to test commit....")
 
 if __name__ == "__main__":
     app.run(debug=True)
